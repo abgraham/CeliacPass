cb074517445d9bbf2efd08c320811f6a82ece9a5
Annie Graham <abgraham@stanford.edu>
2016-10-20 18:33:13 +0200

    Adds python program used to create countries NSArray

---
+++ createCountriesNSArray.py
@@ -0,0 +1,12 @@
+f = open('Countries.txt')
+countriesList = []
+for line in f: 
+    countriesList.append(line.strip())
+
+arrayString = ''
+for country in countriesList:
+    arrayString += "@\""+country+"\", "
+
+arrayString += "NULL"
+
+print arrayString
